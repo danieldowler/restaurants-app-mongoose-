6189f252c0b0f10be33757f811573c5c6a3ca794
Daniel Dowler <shaolinsamurai1988@gmail.com>
2017-10-05 22:08:16 -0600

    cmon

--- test/test.spec.js
+++ test/test.spec.js
@@ -1,12 +1,10 @@
 const should = require('chai').should();
-const isEqual = require('../isEqual');
 const mongoose = require ('mongoose');
 const mocha = require('mocha');
 
 describe('isEqual', function () {
 
     it('should return false if string is empty', function () {
-        isEqual(1, 1)
     });
 }
 )
